7aef43056e1cda0a3c71ec2e153fb8fdd03804e8
github username <github email address>
2019-05-10 13:31:38 -0600

    add procfile

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const apiKey = process.env.API_KEY;
+
+app.set("port", process.env.PORT || 3000);
 
 app.get("/", function (request, response) {
-  const apiKey = process.env.API_KEY;
-  response.send(apiKey);
+
+  response.status(200).json({apiKey});
 })
 
-app.listen(process.env.PORT || 3000);
+app.listen(app.get("port"), () => apiKey);
