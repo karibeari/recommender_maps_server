7c4403d7a5f31abb2591395e61527c8d4819b975
github username <github email address>
2019-05-10 13:50:19 -0600

    fetch maps

--- index.js
+++ index.js
@@ -1,10 +1,18 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const fetch = require("node-fetch");
+const apiKey = process.env.API_KEY;
 
-app.get("/", function (request, response) {
-  const apiKey = process.env.API_KEY;
-  response.send(apiKey);
+const baseUrl = `https://maps.googleapis.com/maps/api/js?key=${apiKey}&callback=initMap`
+
+function getMaps() {
+  return fetch(baseUrl).then(resp => resp.json())
+}
+
+app.get("/", async function (request, response) {
+  const maps = awaiat getMaps()
+  response.send(maps);
 })
 
 app.listen(process.env.PORT || 3000);
