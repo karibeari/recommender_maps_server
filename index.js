c9db6ca42a82193292df2ccc59ea20528618e8d7
github username <github email address>
2019-05-10 13:34:58 -0600

    add dotenv

--- index.js
+++ index.js
@@ -1,13 +1,10 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
-const apiKey = process.env.API_KEY;
-
-app.set("port", process.env.PORT || 3000);
 
 app.get("/", function (request, response) {
-
-  response.status(200).json({apiKey});
+  const apiKey = process.env.API_KEY;
+  response.send(apiKey);
 })
 
-app.listen(app.get("port"), () => apiKey);
+app.listen(process.env.PORT || 3000);
